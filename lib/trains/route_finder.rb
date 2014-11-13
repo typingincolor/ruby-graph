e25ee92f4a04a2be2676c4219c8e748dbdfb7958
Andrew Braithwaite <andrew.braithwaite@laterooms.com>
2014-11-13 12:22:45 -0200

    more simplification

--- lib/trains/route_finder.rb
+++ lib/trains/route_finder.rb
@@ -24,7 +24,7 @@ class RouteFinder
     end
 
     routes.each do |route|
-     if route[:town] == end_point
+      if route[:town] == end_point
         visited[:list].push route[:town]
         visited[:distance] = visited[:distance] + route[:distance]
         result.push({:route => Route.new(*visited[:list].clone), :distance => visited[:distance]})
