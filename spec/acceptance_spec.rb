d925e79e191c8284c657c2dc5b2be9f75c1ba6ce
Andrew Braithwaite <andrew.braithwaite@laterooms.com>
2014-11-14 08:40:26 -0200

    tidyup tests

--- spec/acceptance_spec.rb
+++ spec/acceptance_spec.rb
@@ -6,7 +6,7 @@ describe 'acceptance tests' do
     route = Route.new('A', 'B', 'C')
     traveller = Traveller.new(graph, route)
 
-    expect(traveller.travel).to eq({distance: 9})
+    expect(traveller.travel).to eq(distance: 9)
   end
 
   it 'passes test case 2' do
@@ -14,7 +14,7 @@ describe 'acceptance tests' do
     route = Route.new('A', 'D')
     traveller = Traveller.new(graph, route)
 
-    expect(traveller.travel).to eq({distance: 5})
+    expect(traveller.travel).to eq(distance: 5)
   end
 
   it 'passes test case 3' do
@@ -22,7 +22,7 @@ describe 'acceptance tests' do
     route = Route.new('A', 'D', 'C')
     traveller = Traveller.new(graph, route)
 
-    expect(traveller.travel).to eq({distance: 13})
+    expect(traveller.travel).to eq(distance: 13)
   end
 
   it 'passes test case 4' do
@@ -30,7 +30,7 @@ describe 'acceptance tests' do
     route = Route.new('A', 'E', 'B', 'C', 'D')
     traveller = Traveller.new(graph, route)
 
-    expect(traveller.travel).to eq({distance: 22})
+    expect(traveller.travel).to eq(distance: 22)
   end
 
   it 'passes test case 5' do
@@ -46,13 +46,13 @@ describe 'acceptance tests' do
     finder1 = RoutesWithSpecificNumberOfStopsFinder.new graph, 'C', 'C', 3
     finder2 = RoutesWithSpecificNumberOfStopsFinder.new graph, 'C', 'C', 2
 
-    result = finder1.find().concat finder2.find()
+    result = finder1.find.concat finder2.find
 
     expect(result.size).to eq(2)
 
     expected = [
-        Route.new('C', 'D', 'C'),
-        Route.new('C', 'E', 'B', 'C')
+      Route.new('C', 'D', 'C'),
+      Route.new('C', 'E', 'B', 'C')
     ]
 
     expect(result).to match_array(expected)
@@ -62,13 +62,13 @@ describe 'acceptance tests' do
     graph = Graph.new('AB5,BC4,CD8,DC8,DE6,AD5,CE2,EB3,AE7')
     finder = RoutesWithSpecificNumberOfStopsFinder.new graph, 'A', 'C', 4
 
-    result = finder.find()
+    result = finder.find
     expect(result.size).to eq(3)
 
     expected = [
-        Route.new('A', 'B', 'C', 'D', 'C'),
-        Route.new('A', 'D', 'C', 'D', 'C'),
-        Route.new('A', 'D', 'E', 'B', 'C')
+      Route.new('A', 'B', 'C', 'D', 'C'),
+      Route.new('A', 'D', 'C', 'D', 'C'),
+      Route.new('A', 'D', 'E', 'B', 'C')
     ]
 
     expect(result).to match_array(expected)
@@ -77,33 +77,34 @@ describe 'acceptance tests' do
   it 'passes test case 8' do
     graph = Graph.new('AB5,BC4,CD8,DC8,DE6,AD5,CE2,EB3,AE7')
     finder = ShortestRouteFinder.new graph, 'A', 'C'
-    route = finder.find()
-    expect(route).to eq({route: Route.new('A', 'B', 'C'), distance: 9})
+    route = finder.find
+    expect(route).to eq(route: Route.new('A', 'B', 'C'), distance: 9)
   end
 
   it 'passes test case 9' do
     graph = Graph.new('AB5,BC4,CD8,DC8,DE6,AD5,CE2,EB3,AE7')
     finder = ShortestRouteFinder.new graph, 'B', 'B'
-    route = finder.find()
+    route = finder.find
 
-    expect(route).to eq({route: Route.new('B', 'C', 'E', 'B'), distance: 9})
+    expect(route).to eq(route: Route.new('B', 'C', 'E', 'B'), distance: 9)
   end
 
   it 'passes test case 10' do
     graph = Graph.new('AB5,BC4,CD8,DC8,DE6,AD5,CE2,EB3,AE7')
     finder = RoutesWithMaximumDistanceFinder.new graph, 'C', 'C', 30
-    routes = finder.find()
+    routes = finder.find
 
     expect(routes.size).to eq(7)
 
     expected = [
-        {route: Route.new('C', 'D', 'C'), distance: 16},
-        {route: Route.new('C', 'E', 'B', 'C'), distance: 9},
-        {route: Route.new('C', 'E', 'B', 'C', 'D', 'C'), distance: 25},
-        {route: Route.new('C', 'D', 'C', 'E', 'B', 'C'), distance: 25},
-        {route: Route.new('C', 'D', 'E', 'B', 'C'), distance: 21},
-        {route: Route.new('C', 'E', 'B', 'C', 'E', 'B', 'C'), distance: 18},
-        {route: Route.new('C', 'E', 'B', 'C', 'E', 'B', 'C', 'E', 'B', 'C'), distance: 27}
+      { route: Route.new('C', 'D', 'C'), distance: 16 },
+      { route: Route.new('C', 'E', 'B', 'C'), distance: 9 },
+      { route: Route.new('C', 'E', 'B', 'C', 'D', 'C'), distance: 25 },
+      { route: Route.new('C', 'D', 'C', 'E', 'B', 'C'), distance: 25 },
+      { route: Route.new('C', 'D', 'E', 'B', 'C'), distance: 21 },
+      { route: Route.new('C', 'E', 'B', 'C', 'E', 'B', 'C'), distance: 18 },
+      { route: Route.new('C', 'E', 'B', 'C', 'E', 'B', 'C', 'E', 'B', 'C'),
+        distance: 27 }
     ]
 
     expect(routes).to match_array(expected)
--- spec/graph_spec.rb
+++ spec/graph_spec.rb
@@ -5,42 +5,46 @@ describe Graph do
     graph = Graph.new('AB5')
 
     expect(graph.nodes.size).to eq(1)
-    expect(graph.nodes['A']).to eq([{:town => 'B', :distance => 5}])
+    expect(graph.nodes['A']).to eq([town: 'B', distance: 5])
   end
 
   it 'can build two routes starting at the same town' do
     graph = Graph.new('AB5,AC4')
 
     expect(graph.nodes.size).to eq(1)
-    expect(graph.nodes['A']).to eq([{:town => 'B', :distance => 5}, {:town => 'C', :distance => 4}])
+    expect(graph.nodes['A']).to eq([{ town: 'B', distance: 5 },
+                                    { town: 'C', distance: 4 }])
   end
 
   it 'does not allow a duplicate route to be created' do
-    expect {
+    expect do
       Graph.new('AB1,AB5')
-    }.to raise_error(DuplicateRouteException)
+    end.to raise_error(DuplicateRouteException)
   end
 
   it 'can build a graph with routes starting at two towns' do
     graph = Graph.new('AB5,AC4,CB6')
 
     expect(graph.nodes.size).to eq(2)
-    expect(graph.nodes['A']).to eq([{:town => 'B', :distance => 5}, {:town => 'C', :distance => 4}])
-    expect(graph.nodes['C']).to eq([{:town => 'B', :distance => 6}])
+    expect(graph.nodes['A']).to eq([{ town: 'B', distance: 5 },
+                                    { town: 'C', distance: 4 }])
+    expect(graph.nodes['C']).to eq([{ town: 'B', distance: 6 }])
   end
 
   it 'can find routes starting at a valid town' do
     graph = Graph.new('AB5,AC4,CB6')
 
-    expect(graph.get_routes_starting_at 'A').to eq([{:town => 'B', :distance => 5}, {:town => 'C', :distance => 4}])
+    expect(graph.get_routes_starting_at 'A').to eq([{ town: 'B', distance: 5 },
+                                                    { town: 'C', distance: 4 }])
   end
 
-  it 'throws a NoRoutesFoundException when asking for routes starting at an invalid town' do
+  it 'throws a NoRoutesFoundException when asking for routes
+      starting at an invalid town' do
     graph = Graph.new('AB5,AC4,CB6')
 
-    expect {
+    expect do
       graph.get_routes_starting_at 'D'
-    }.to raise_error(NoRoutesFoundException)
+    end.to raise_error(NoRoutesFoundException)
   end
 
   it 'can find a route between two towns' do
@@ -48,14 +52,15 @@ describe Graph do
 
     route = graph.route?('A', 'B')
 
-    expect(route).to eq({:town => 'B', :distance => 5})
+    expect(route).to eq(town: 'B', distance: 5)
   end
 
-  it 'throws NoSuchRouteException when it tries to find a route between two towns and none exists' do
+  it 'throws NoSuchRouteException when it tries to find a route
+      between two towns and none exists' do
     graph = Graph.new('AB5,AC4,CB6,CD5')
 
-    expect {
+    expect do
       graph.route?('A', 'D')
-    }.to raise_error(NoSuchRouteException)
+    end.to raise_error(NoSuchRouteException)
   end
 end
