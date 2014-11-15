27e387a3ea31c833fd27ee9a1484c2d8987aa136
Andrew Braithwaite <typingincolor@users.noreply.github.com>
2014-11-15 22:22:02 +0000

    Update route_finder.rb
    Remove duplicate code

--- lib/trains/route_finder.rb
+++ lib/trains/route_finder.rb
@@ -6,31 +6,6 @@ class RouteFinder < RouteFinderTemplate
     @end_point = end_point
   end
 
-  def find
-    visited = VisitedTowns.new @start_point
-
-    result = search(visited, false)
-
-    fail NoSuchRouteException if result.size == 0
-
-    result
-  end
-
-  def search(visited, left_start)
-    result = []
-    begin
-      routes = @graph.get_routes_starting_at visited.last
-    rescue NoRoutesFoundException
-      return result
-    end
-
-    handle_at_endpoint routes, visited, result
-
-    handle_not_at_endpoint routes, left_start, visited, result
-
-    result
-  end
-
   def handle_at_endpoint(routes, visited, result)
     routes.each do |route|
       next if route[:town] != @end_point
